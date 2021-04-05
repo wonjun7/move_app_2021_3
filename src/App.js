72c911e5db9e2544c8e4d61785e7c55feb8b56ec
정원준 <dnjswnsakdmf@gmail.com>
2021-04-05 17:09:29 +0900

    last commit code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './App.css'
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About'
+import Home from './routes/Home'
+import Detail from "./routes/Detail"
+import Navigation from './components/Navigation'
 
-class App extends React.Component {
-  render() {
-    return (
-    <div></div>
-    )
-  }
+
+function App() {
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail}/>
+    </HashRouter>
+  );
 }
-
 export default App;
